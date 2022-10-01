f3f55fa3f0aed4db6aaaaf5079f07c083729eacc
mys1024 <mys.1024@outlook.com>
2022-10-01 20:26:36 +0800

    test: add tests for `util/resolveAlias()`

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,80 @@
 import { describe, expect, it } from 'vitest'
+import { resolve } from 'pathe'
+import { resolveAlias } from '../src/util'
+import { Aliases } from '../src/types'
 
-describe.todo('TODO', () => {
-  it('basic', async () => {
-    expect(1 + 1).toBe(2)
+describe('util/resolveAlias()', () => {
+  const aliases: Aliases = {
+    '~/': 'src/',
+    '@/': 'src/components/',
+    'conf': 'src/config.js'
+  }
+
+  it('Absolute base dir on POSIX', async () => {
+    expect(resolveAlias(aliases, '/path/to/project')).toMatchInlineSnapshot(`
+      {
+        "@/": "/path/to/project/src/components/",
+        "conf": "/path/to/project/src/config.js",
+        "~/": "/path/to/project/src/",
+      }
+    `)
+  })
+
+  it('Absolute base dir on Windows', async () => {
+    expect(resolveAlias(aliases, 'C:\\path\\to\\project')).toMatchInlineSnapshot(`
+      {
+        "@/": "C:/path/to/project/src/components/",
+        "conf": "C:/path/to/project/src/config.js",
+        "~/": "C:/path/to/project/src/",
+      }
+    `)
+  })
+
+  it('Relative base dir on POSIX', async () => {
+    const cwd = resolve(process.cwd())
+    const resolvedAliases = resolveAlias(aliases, 'path/to/project')
+    const relativeAliases: Record<string, string> = {}
+    for (const alias of Object.keys(resolvedAliases)) {
+      relativeAliases[alias] = resolvedAliases[alias].replace(`${cwd}/`, '')
+    }
+    expect(relativeAliases).toMatchInlineSnapshot(`
+      {
+        "@/": "path/to/project/src/components/",
+        "conf": "path/to/project/src/config.js",
+        "~/": "path/to/project/src/",
+      }
+    `)
+  })
+
+  it('Relative base dir on Windows', async () => {
+    const cwd = resolve(process.cwd())
+    const resolvedAliases = resolveAlias(aliases, 'path\\to\\project')
+    const relativeAliases: Record<string, string> = {}
+    for (const alias of Object.keys(resolvedAliases)) {
+      relativeAliases[alias] = resolvedAliases[alias].replace(`${cwd}/`, '')
+    }
+    expect(relativeAliases).toMatchInlineSnapshot(`
+      {
+        "@/": "path/to/project/src/components/",
+        "conf": "path/to/project/src/config.js",
+        "~/": "path/to/project/src/",
+      }
+    `)
+  })
+
+  it('Complex relative base dir', async () => {
+    const cwd = resolve(process.cwd())
+    const resolvedAliases = resolveAlias(aliases, './path/to/../to/project')
+    const relativeAliases: Record<string, string> = {}
+    for (const alias of Object.keys(resolvedAliases)) {
+      relativeAliases[alias] = resolvedAliases[alias].replace(`${cwd}/`, '')
+    }
+    expect(relativeAliases).toMatchInlineSnapshot(`
+      {
+        "@/": "path/to/project/src/components/",
+        "conf": "path/to/project/src/config.js",
+        "~/": "path/to/project/src/",
+      }
+    `)
   })
 })
